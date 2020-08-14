ad8bf4715e2349d1c251dcd57c5e061b7ccee4ee
Yuanfeng Dai <yuanfeng.dai@163.sufe.edu.cn>
2020-08-14 10:54:50 +0800

    all window management related apis are realized

--- frontend/index.js
+++ frontend/index.js
@@ -6,19 +6,41 @@ function hideAboutPage (document) {
     document.getElementById('mainpage').style.visibility = 'visible';
 }
 
-function clickSection (section_name, document) {
+function clickSection (section_name, document, btnId) {
     console.log(section_name)
+    let sections = document.getElementsByClassName("content-section")
+    Array.prototype.forEach.call(sections, function(section){
+        if (section.id !== section_name) {
+            section.classList.add('hide')
+        }
+    })
     document.getElementById(section_name).classList.remove('hide')
+    let navBtns = document.getElementsByClassName('nav-button')
+    Array.prototype.forEach.call(navBtns, function(btn){
+        if (btn.id !== btnId) {
+            btn.classList.remove('pressed')
+        }
+    })
+    document.getElementById(btnId).classList.add('pressed')
 }
 
 function showDemoContent (btnId, document) {
-    console.log(btnId)
-    let verticalLines = document.getElementsByClassName('demo-vertical-line');
+    const btnIdSectionIdMap = {
+        windowCreationDemoBtn: 'windowCreation',
+        windowStateDemoBtn: 'windowManageState',
+        windowBlurFocusDemoBtn: 'windowBlurFocus',
+        windowFramelessBtn: 'windowFrameless'
+    }
+    let section = document.getElementsByClassName("demo-wrapper " + btnIdSectionIdMap[btnId])[0]
+    let demoBoxWrapper = section.getElementsByClassName('demo-box-wrapper')[0]
+    let verticalLines = section.getElementsByClassName('demo-vertical-line');
     Array.prototype.forEach.call(verticalLines, function(line){
         if (line.classList.contains('expand')) {
             line.classList.remove('expand')
+            demoBoxWrapper.classList.remove('expand')
         } else {
             line.classList.add('expand')
+            demoBoxWrapper.classList.add('expand')
         }
     })
 }
--- preload.js
+++ preload.js
@@ -5,12 +5,69 @@ const path = require('path')
 
 window.functions = {
     openNewWindow: openNewWindow,
+    windowListenReszie: windowListenReszie,
+    windowListenBlurFocus: windowListenBlurFocus,
+    showFramelessWindow: showFramelessWindow,
 }
 
 function openNewWindow() {
     const modalPath = path.join('file://', __dirname, 'sections/windowModal.html')
     let win = new BrowserWindow({width: 400, height: 320})
     win.on('close', () => {win=null})
+    win.loadURL(modalPath)
+    win.show()
+    return win
+}
+
+function windowListenReszie() {
+    const modalPath = path.join('file://', __dirname, 'sections/windowModal.html')
+    let win = new BrowserWindow({width: 400, height: 320})
+    win.on('resize', updateReply)
+    win.on('move', updateReply)
+    win.on('close', closed)
+    win.loadURL(modalPath)
+    win.show()
+
+    function updateReply () {
+        const manageWindowReply = document.getElementById('manage-window-reply')
+        const message = `Size: ${win.getSize()} Position: ${win.getPosition()}`
+        manageWindowReply.innerText = message
+    }
+
+    function closed () {
+        win = null;
+        document.getElementById('manage-window-reply').innerText = 'Closed!'
+    }
+}
+
+function windowListenBlurFocus() {
+    const modalPath = path.join('file://', __dirname, 'sections/windowModal.html')
+    let win = new BrowserWindow({width: 400, height: 320})
+    let focusBtn = document.getElementById('focusBtn')
+    function hideFocusBtn () {
+        focusBtn.style.visibility = "hidden"
+    }
+    function showFocusBtn () {
+        focusBtn.style.visibility = 'visible'
+    }
+    function focus () {
+        win.focus()
+    }
+    win.on('blur', showFocusBtn)
+    win.on('focus', hideFocusBtn)
+    win.on('close', ()=>{
+        hideFocusBtn()
+        win = null
+    })
+    win.loadURL(modalPath)
+    win.show()
+    focusBtn.addEventListener('click', focus)    
+}
+
+function showFramelessWindow () {
+    const modalPath = path.join('file://', __dirname, 'sections/windowModal.html')
+    let win = new BrowserWindow({width: 400, height: 320, frame: false})
+    win.on('close', ()=>{win=null})
     win.loadURL(modalPath)
     win.show()
 }
